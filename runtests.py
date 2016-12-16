7d9e23c2e12412b49ec16a59cc43245f3fc8f80e
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2016-12-16 23:47:18 +0100

    Tests for aiohttp will only run with Python >= 3.5.

--- runtests.py
+++ runtests.py
@@ -8,9 +8,22 @@ def runtests(args=None):
     if not args:
         args = []
 
+    major, minor = sys.version_info[:2]
+
+    python35 = False
+
+    if major == 3 and minor >= 5:
+        python35 = True
+
+        import pip
+        pip.main(['install', 'aiohttp'])
+
     if not any(a for a in args[1:] if not a.startswith('-')):
         args.append('tests')
         args.append('mocket')
+
+        if python35:
+            args.append('tests35')
 
     sys.exit(pytest.main(args))
 
