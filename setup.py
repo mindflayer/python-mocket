1bac329399acf179ae24f129c1295661df78da4a
Giorgio Salluzzo <giorgio.salluzzo@planet.com>
2016-12-30 12:19:28 +0100

    Releasing new version.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = open(os.path.join(os.path.dirname(__file__), 'test_requirements.
 
 setup(
     name='mocket',
-    version='1.5.0',
+    version='1.5.1',
     # author='Andrea de Marco, Giorgio Salluzzo',
     author='Giorgio Salluzzo',
     # author_email='24erre@gmail.com, giorgio.salluzzo@gmail.com',
