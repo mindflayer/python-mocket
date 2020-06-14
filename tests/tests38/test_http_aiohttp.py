7a4391c7de574059810ba8013944b8175052114f
Anas <anas.el.amraoui@live.com>
2020-06-14 12:14:17 +0300

    Added async_httprettified test

--- tests/tests38/test_http_aiohttp.py
+++ tests/tests38/test_http_aiohttp.py
@@ -7,7 +7,7 @@ import async_timeout
 
 from mocket.mocket import Mocket, async_mocketize
 from mocket.mockhttp import Entry
-from mocket.plugins.httpretty import HTTPretty, httprettified
+from mocket.plugins.httpretty import HTTPretty, async_httprettified, httprettified
 
 
 class AioHttpEntryTestCase(IsolatedAsyncioTestCase):
@@ -53,21 +53,17 @@ class AioHttpEntryTestCase(IsolatedAsyncioTestCase):
 
         self.assertEqual(len(Mocket._requests), 2)
 
-    # @httprettified
-    # def test_httprettish_session(self):
-    #     url = 'https://httpbin.org/ip'
-    #     HTTPretty.register_uri(
-    #         HTTPretty.GET,
-    #         url,
-    #         body=json.dumps(dict(origin='127.0.0.1')),
-    #     )
-    #
-    #     async def main(l):
-    #         async with aiohttp.ClientSession(loop=l) as session:
-    #             with async_timeout.timeout(3):
-    #                 async with session.get(url) as get_response:
-    #                     assert get_response.status == 200
-    #                     assert await get_response.text() == '{"origin": "127.0.0.1"}'
-    #     loop = asyncio.get_event_loop()
-    #     loop.set_debug(True)
-    #     loop.run_until_complete(main(loop))
+    @async_httprettified
+    async def test_httprettish_session(self):
+        url = 'https://httpbin.org/ip'
+        HTTPretty.register_uri(
+            HTTPretty.GET,
+            url,
+            body=json.dumps(dict(origin='127.0.0.1')),
+        )
+
+        async with aiohttp.ClientSession() as session:
+            with async_timeout.timeout(3):
+                async with session.get(url) as get_response:
+                    assert get_response.status == 200
+                    assert await get_response.text() == '{"origin": "127.0.0.1"}'
