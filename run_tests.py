9872c119464d36e33c6f6f56b86f50224f7b79b6
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2021-01-03 17:46:05 +0100

    Yet another refactor (#135)

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import os
 import sys
 
 
@@ -11,24 +10,10 @@ def main(args=None):
 
     major, minor = sys.version_info[:2]
 
-    python35 = False
-
-    extras = ["xxhash"]
-
-    # aiohttp available on Python >=3.5
-    if major == 3 and minor >= 5:
-        python35 = True
-
-        extras += ["aiohttp", "async_timeout"]
-
-    os.system("pipenv run pip install {}".format(" ".join(extras)))
-
     if not any(a for a in args[1:] if not a.startswith("-")):
         args.append("tests/main")
         args.append("mocket")
-
-        if python35:
-            args.append("tests/tests35")
+        args.append("tests/tests35")
 
         if major == 3 and minor >= 8:
             args.append("tests/tests38")
