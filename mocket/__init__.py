cdde193f0685cad4a1304ba2d0bfd7e560ed224c
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2021-01-05 17:49:13 +0100

    Refactor.

--- mocket/__init__.py
+++ mocket/__init__.py
@@ -1,9 +1,4 @@
-try:
-    # Py2
-    from mocket import Mocket, MocketEntry, Mocketizer, mocketize
-except ImportError:
-    # Py3
-    from mocket.mocket import Mocket, MocketEntry, Mocketizer, mocketize
+from mocket.mocket import Mocket, MocketEntry, Mocketizer, mocketize
 
 __all__ = ("mocketize", "Mocket", "MocketEntry", "Mocketizer")
 
