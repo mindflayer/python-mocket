35b08a30f2de696309d959e654789af8ce8299aa
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2021-01-15 17:22:35 +0100

    Use MocketSocket as context manager (#141)
    * Adding support for using a `socket` as a context manager as requested by #139.
    * Closing real socket.
    * Bump version.

--- mocket/__init__.py
+++ mocket/__init__.py
@@ -2,4 +2,4 @@ from mocket.mocket import Mocket, MocketEntry, Mocketizer, mocketize
 
 __all__ = ("mocketize", "Mocket", "MocketEntry", "Mocketizer")
 
-__version__ = "3.9.38"
+__version__ = "3.9.39"
