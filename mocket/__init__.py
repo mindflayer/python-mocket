68af0c1889bcd4afd3d7ddb45dfaac980cad3633
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2021-01-21 17:36:56 +0100

    Update __init__.py

--- mocket/__init__.py
+++ mocket/__init__.py
@@ -2,4 +2,4 @@ from mocket.mocket import Mocket, MocketEntry, Mocketizer, mocketize
 
 __all__ = ("mocketize", "Mocket", "MocketEntry", "Mocketizer")
 
-__version__ = "3.9.39"
+__version__ = "3.9.40"
