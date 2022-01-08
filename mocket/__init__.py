93eff064900ac1051eea5ad47c07783a334ce62e
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2022-01-08 17:32:20 +0100

    Fix import lines.

--- mocket/__init__.py
+++ mocket/__init__.py
@@ -1,5 +1,5 @@
-from mocket.async_mocket import async_mocketize
-from mocket.mocket import Mocket, MocketEntry, Mocketizer, mocketize
+from .async_mocket import async_mocketize
+from .mocket import Mocket, MocketEntry, Mocketizer, mocketize
 
 __all__ = ("async_mocketize", "mocketize", "Mocket", "MocketEntry", "Mocketizer")
 
