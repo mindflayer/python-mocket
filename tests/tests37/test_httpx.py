2ec367ebf186fe4f75aa8b26a8316c5694f2b627
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2022-08-18 22:01:54 +0100

    Improving tests for `httpx`. (#186)

--- tests/tests37/test_httpx.py
+++ tests/tests37/test_httpx.py
@@ -3,14 +3,14 @@ import json
 import httpx
 import pytest
 
-from mocket import async_mocketize
+from mocket import Mocketizer, async_mocketize
 from mocket.mockhttp import Entry
 
 
 @pytest.mark.asyncio
 @async_mocketize
-async def test_httpx():
-    url = "https://example.org/"
+async def test_httpx_decorator():
+    url = "https://bar.foo/"
     data = {"message": "Hello"}
 
     Entry.single_register(
@@ -21,6 +21,30 @@ async def test_httpx():
     )
 
     async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+        assert response.json() == data
+
+
+@pytest.fixture
+def httpx_client() -> httpx.AsyncClient:
+    with Mocketizer():
+        yield httpx.AsyncClient()
+
+
+@pytest.mark.asyncio
+async def test_httpx_fixture(httpx_client):
+    url = "https://foo.bar/"
+    data = {"message": "Hello"}
+
+    Entry.single_register(
+        Entry.GET,
+        url,
+        body=json.dumps(data),
+        headers={"content-type": "application/json"},
+    )
+
+    async with httpx_client as client:
         response = await client.get(url)
 
         assert response.json() == data
