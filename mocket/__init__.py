d8391a283caf3f1dcf994324af3d6d0d7b91d774
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2023-07-30 15:14:13 +0200

    Switch from `setup.py` to `pyproject.toml`.

--- mocket/__init__.py
+++ mocket/__init__.py
@@ -2,5 +2,3 @@ from .async_mocket import async_mocketize
 from .mocket import Mocket, MocketEntry, Mocketizer, mocketize
 
 __all__ = ("async_mocketize", "mocketize", "Mocket", "MocketEntry", "Mocketizer")
-
-__version__ = "3.11.1"
