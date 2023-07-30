c6f7f16ee4897b5c731b994e75ccb3198f74ff8b
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2023-07-30 15:30:19 +0200

    Switch to Hatch.

--- mocket/__init__.py
+++ mocket/__init__.py
@@ -2,3 +2,5 @@ from .async_mocket import async_mocketize
 from .mocket import Mocket, MocketEntry, Mocketizer, mocketize
 
 __all__ = ("async_mocketize", "mocketize", "Mocket", "MocketEntry", "Mocketizer")
+
+__version__ = "3.11.1"
