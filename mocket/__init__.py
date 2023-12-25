91cfac0ea60a04f8415a81e5cd8b113bd59bf0cb
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2023-12-25 13:05:39 +0100

    Excluding venvs. (#212)

--- mocket/__init__.py
+++ mocket/__init__.py
@@ -3,4 +3,4 @@ from .mocket import Mocket, MocketEntry, Mocketizer, mocketize
 
 __all__ = ("async_mocketize", "mocketize", "Mocket", "MocketEntry", "Mocketizer")
 
-__version__ = "3.12.1"
+__version__ = "3.12.2"
