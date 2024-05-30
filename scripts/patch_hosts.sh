501088ee1755a222c473b74cfb457ae534478bb0
Giorgio Salluzzo <giorgio.salluzzo@satellogic.com>
2024-05-30 09:28:17 +0200

    Clean hosts file before adding the IP.

--- scripts/patch_hosts.sh
+++ scripts/patch_hosts.sh
@@ -1,4 +1,4 @@
-sudo cp /etc/hosts /etc/hosts.mocket
+sudo grep -v httpbin.local /etc/hosts | sudo tee /etc/hosts.mocket
 export CONTAINER_ID=$(docker compose ps -q proxy)
 export CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
 echo "$CONTAINER_IP httpbin.local" | sudo tee -a /etc/hosts.mocket
