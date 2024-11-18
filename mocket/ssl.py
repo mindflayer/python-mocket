4dc38cafa50b499bf783998e55a87e8446b2dce5
betaboon <betaboon@0x80.ninja>
2024-11-18 10:16:24 +0100

    refactor: type SuperFakeSSLContext and FakeSSLContext

--- mocket/ssl.py
+++ mocket/ssl.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+
+from typing import Any
+
+from mocket.socket import MocketSocket
+
+
 class SuperFakeSSLContext:
     """For Python 3.6 and newer."""
 
     class FakeSetter(int):
-        def __set__(self, *args):
+        def __set__(self, *args: Any) -> None:
             pass
 
     minimum_version = FakeSetter()
@@ -24,33 +31,36 @@ class FakeSSLContext(SuperFakeSSLContext):
     _check_hostname = False
 
     @property
-    def check_hostname(self):
+    def check_hostname(self) -> bool:
         return self._check_hostname
 
     @check_hostname.setter
-    def check_hostname(self, _):
+    def check_hostname(self, _: bool) -> None:
         self._check_hostname = False
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         self._set_dummy_methods()
 
-    def _set_dummy_methods(self):
-        def dummy_method(*args, **kwargs):
+    def _set_dummy_methods(self) -> None:
+        def dummy_method(*args: Any, **kwargs: Any) -> Any:
             pass
 
         for m in self.DUMMY_METHODS:
             setattr(self, m, dummy_method)
 
     @staticmethod
-    def wrap_socket(sock, *args, **kwargs):
+    def wrap_socket(sock: MocketSocket, *args: Any, **kwargs: Any) -> MocketSocket:
         sock.kwargs = kwargs
         sock._secure_socket = True
         return sock
 
     @staticmethod
-    def wrap_bio(incoming, outcoming, *args, **kwargs):
-        from mocket.socket import MocketSocket
-
+    def wrap_bio(
+        incoming: Any,  # _ssl.MemoryBIO
+        outgoing: Any,  # _ssl.MemoryBIO
+        server_side: bool = False,
+        server_hostname: str | bytes | None = None,
+    ) -> MocketSocket:
         ssl_obj = MocketSocket()
-        ssl_obj._host = kwargs["server_hostname"]
+        ssl_obj._host = server_hostname
         return ssl_obj
