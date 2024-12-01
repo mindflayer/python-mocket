f264359d26a608cb07c8257c4004b95ad5a4bf0e
betaboon <betaboon@0x80.ninja>
2024-12-01 11:53:43 +0100

    chore: mocket.core.io - add type annotations

--- mocket/core/io.py
+++ mocket/core/io.py
@@ -1,17 +1,22 @@
 import io
 import os
 
+from typing_extensions import Buffer
+
 from mocket.core.mocket import Mocket
+from mocket.core.types import Address
 
 
 class MocketSocketIO(io.BytesIO):
-    def __init__(self, address) -> None:
+    def __init__(self, address: Address) -> None:
         self._address = address
         super().__init__()
 
-    def write(self, content):
-        super().write(content)
+    def write(self, content: Buffer) -> int:
+        bytes_written = super().write(content)
 
         _, w_fd = Mocket.get_pair(self._address)
         if w_fd:
-            os.write(w_fd, content)
+            return os.write(w_fd, content)
+
+        return bytes_written
