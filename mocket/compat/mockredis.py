fd2edda300f67d634d1c8a2f0b127540b9fb2693
betaboon <betaboon@0x80.ninja>
2024-12-01 13:49:33 +0100

    refactor: migrate mocket.compat.mockredis to use mocket.redis

--- mocket/compat/mockredis.py
+++ mocket/compat/mockredis.py
@@ -1,91 +1,117 @@
-from itertools import chain
+from __future__ import annotations
 
-from mocket.compat.entry import MocketEntry
-from mocket.core.compat import (
-    decode_from_bytes,
-    encode_to_bytes,
-    shsplit,
-)
+from typing import Sequence
+
 from mocket.core.mocket import Mocket
+from mocket.core.types import Address
+from mocket.redis import MocketRedisEntry, MocketRedisResponse
+
+DEFAULT_ADDRESS = ("localhost", 6379)
 
 
-class Request:
-    def __init__(self, data):
-        self.data = data
-
-
-class Response:
-    def __init__(self, data=None):
-        self.data = Redisizer.redisize(data or OK)
-
-
-class Redisizer(bytes):
-    @staticmethod
-    def tokens(iterable):
-        iterable = [encode_to_bytes(x) for x in iterable]
-        return [f"*{len(iterable)}".encode()] + list(
-            chain(*zip([f"${len(x)}".encode() for x in iterable], iterable))
+class Entry(MocketRedisEntry):
+    def __init__(
+        self,
+        addr: Address | None,
+        command: str | bytes,
+        responses: Sequence[MocketRedisResponse | Exception],
+    ) -> None:
+        super().__init__(
+            address=addr or DEFAULT_ADDRESS,
+            command=command,
+            responses=responses,
         )
 
-    @staticmethod
-    def redisize(data):
-        def get_conversion(t):
-            return {
-                dict: lambda x: b"\r\n".join(
-                    Redisizer.tokens(list(chain(*tuple(x.items()))))
-                ),
-                int: lambda x: f":{x}".encode(),
-                str: lambda x: "${}\r\n{}".format(len(x.encode("utf-8")), x).encode(
-                    "utf-8"
-                ),
-                list: lambda x: b"\r\n".join(Redisizer.tokens(x)),
-            }[t]
-
-        if isinstance(data, Redisizer):
-            return data
-        if isinstance(data, bytes):
-            data = decode_from_bytes(data)
-        return Redisizer(get_conversion(data.__class__)(data) + b"\r\n")
+    @property
+    def command(self) -> list[bytes]:  # type: ignore[override]
+        return self._command_tokens
 
     @staticmethod
-    def command(description, _type="+"):
-        return Redisizer("{}{}{}".format(_type, description, "\r\n").encode("utf-8"))
+    def _convert_response(
+        response: str
+        | bytes
+        | int
+        | list[str]
+        | list[bytes]
+        | dict[str, str]
+        | dict[bytes, bytes]
+        | Exception
+        | MocketRedisResponse,
+    ) -> MocketRedisResponse | Exception:
+        if isinstance(response, (MocketRedisResponse, Exception)):
+            return response
 
-    @staticmethod
-    def error(description):
-        return Redisizer.command(description, _type="-")
-
-
-OK = Redisizer.command("OK")
-QUEUED = Redisizer.command("QUEUED")
-ERROR = Redisizer.error
-
-
-class Entry(MocketEntry):
-    request_cls = Request
-    response_cls = Response
-
-    def __init__(self, addr, command, responses):
-        super().__init__(addr or ("localhost", 6379), responses)
-        d = shsplit(command)
-        d[0] = d[0].upper()
-        self.command = Redisizer.tokens(d)
-
-    def can_handle(self, data):
-        return data.splitlines() == self.command
+        return MocketRedisResponse(data=response)
 
     @classmethod
-    def register(cls, addr, command, *responses):
-        responses = [
-            r if isinstance(r, BaseException) else cls.response_cls(r)
-            for r in responses
-        ]
-        Mocket.register(cls(addr, command, responses))
+    def register(
+        cls,
+        addr: Address | None,
+        command: str | bytes,
+        *responses: str
+        | bytes
+        | int
+        | list[str]
+        | list[bytes]
+        | dict[str, str]
+        | dict[bytes, bytes]
+        | Exception
+        | MocketRedisResponse,
+    ) -> None:
+        cls.register_responses(
+            command=command,
+            responses=responses,
+            addr=addr,
+        )
 
     @classmethod
-    def register_response(cls, command, response, addr=None):
-        cls.register(addr, command, response)
+    def register_response(  # type: ignore[override]
+        cls,
+        command: str | bytes,
+        response: str
+        | bytes
+        | int
+        | list[str]
+        | list[bytes]
+        | dict[str, str]
+        | dict[bytes, bytes]
+        | Exception
+        | MocketRedisResponse,
+        addr: Address | None = None,
+    ) -> None:
+        response = Entry._convert_response(response)
+        entry = cls(
+            addr=addr or DEFAULT_ADDRESS,
+            command=command,
+            responses=[response],
+        )
+        Mocket.register(entry)
 
     @classmethod
-    def register_responses(cls, command, responses, addr=None):
-        cls.register(addr, command, *responses)
+    def register_responses(  # type: ignore[override]
+        cls,
+        command: str | bytes,
+        responses: Sequence[
+            str
+            | bytes
+            | int
+            | list[str]
+            | list[bytes]
+            | dict[str, str]
+            | dict[bytes, bytes]
+            | Exception
+            | MocketRedisResponse
+        ],
+        addr: Address | None = None,
+    ) -> None:
+        _responses = []
+        for response in responses:
+            response = Entry._convert_response(response)
+            _responses.append(response)
+
+        entry = cls(
+            addr=addr or DEFAULT_ADDRESS,
+            command=command,
+            responses=_responses,
+        )
+        Mocket.register(entry)
--- mocket/mockredis.py
+++ mocket/mockredis.py
@@ -1,12 +1,7 @@
-from mocket.compat.mockredis import (
-    ERROR,
-    OK,
-    QUEUED,
-    Entry,
-    Redisizer,
-    Request,
-    Response,
-)
+from mocket.compat.mockredis import Entry
+from mocket.redis import ERROR, OK, QUEUED, Redisizer
+from mocket.redis import MocketRedisRequest as Request
+from mocket.redis import MocketRedisResponse as Response
 
 # NOTE this is here for backwards-compat to keep old import-paths working
 __all__ = [
