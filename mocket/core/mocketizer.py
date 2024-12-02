755f9fc7e5983070824346abfa96f21c34985575
betaboon <betaboon@0x80.ninja>
2024-12-02 10:57:24 +0100

    chore: mocket.core.mocketizer - add type annotations

--- mocket/core/mocketizer.py
+++ mocket/core/mocketizer.py
@@ -1,16 +1,24 @@
+from __future__ import annotations
+
+from types import TracebackType
+from typing import Any
+
+from typing_extensions import Self
+
 from mocket.core.mocket import Mocket
 from mocket.core.mode import MocketMode
+from mocket.core.types import Address, AnyCallable
 from mocket.core.utils import get_mocketize
 
 
 class Mocketizer:
     def __init__(
         self,
-        instance=None,
-        namespace=None,
-        truesocket_recording_dir=None,
-        strict_mode=False,
-        strict_mode_allowed=None,
+        instance: Any | None = None,
+        namespace: str | None = None,
+        truesocket_recording_dir: str | None = None,
+        strict_mode: bool = False,
+        strict_mode_allowed: list[str | Address] | None = None,
     ):
         self.instance = instance
         self.truesocket_recording_dir = truesocket_recording_dir
@@ -23,7 +31,7 @@ class Mocketizer:
                 "Allowed locations are only accepted when STRICT mode is active."
             )
 
-    def enter(self):
+    def enter(self) -> None:
         Mocket.enable(
             namespace=self.namespace,
             truesocket_recording_dir=self.truesocket_recording_dir,
@@ -31,33 +39,49 @@ class Mocketizer:
         if self.instance:
             self.check_and_call("mocketize_setup")
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
         self.enter()
         return self
 
-    def exit(self):
+    def exit(self) -> None:
         if self.instance:
             self.check_and_call("mocketize_teardown")
 
         Mocket.disable()
 
-    def __exit__(self, type, value, tb):
+    def __exit__(
+        self,
+        type_: type[BaseException] | None,
+        value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
         self.exit()
 
-    async def __aenter__(self, *args, **kwargs):
+    async def __aenter__(self) -> Self:
         self.enter()
         return self
 
-    async def __aexit__(self, *args, **kwargs):
+    async def __aexit__(
+        self,
+        type_: type[BaseException] | None,
+        value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
         self.exit()
 
-    def check_and_call(self, method_name):
+    def check_and_call(self, method_name: str) -> None:
         method = getattr(self.instance, method_name, None)
         if callable(method):
             method()
 
     @staticmethod
-    def factory(test, truesocket_recording_dir, strict_mode, strict_mode_allowed, args):
+    def factory(
+        test: AnyCallable,
+        truesocket_recording_dir: str | None,
+        strict_mode: bool,
+        strict_mode_allowed: list[str | Address] | None,
+        args: Any,
+    ) -> Mocketizer:
         instance = args[0] if args else None
         namespace = None
         if truesocket_recording_dir:
@@ -79,13 +103,13 @@ class Mocketizer:
 
 
 def wrapper(
-    test,
-    truesocket_recording_dir=None,
-    strict_mode=False,
-    strict_mode_allowed=None,
-    *args,
-    **kwargs,
-):
+    test: AnyCallable,
+    truesocket_recording_dir: str | None = None,
+    strict_mode: bool = False,
+    strict_mode_allowed: list[str | Address] | None = None,
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
     with Mocketizer.factory(
         test, truesocket_recording_dir, strict_mode, strict_mode_allowed, args
     ):
