a47fa80d78f40ea259a0be5fad9a603ad31c90e5
betaboon <betaboon@0x80.ninja>
2024-12-02 11:00:22 +0100

    chore: mocket.core.async_mocket - add type annotations

--- mocket/core/async_mocket.py
+++ mocket/core/async_mocket.py
@@ -1,15 +1,20 @@
+from __future__ import annotations
+
+from typing import Any
+
 from mocket.core.mocketizer import Mocketizer
+from mocket.core.types import Address, AnyCallable
 from mocket.core.utils import get_mocketize
 
 
 async def wrapper(
-    test,
-    truesocket_recording_dir=None,
-    strict_mode=False,
-    strict_mode_allowed=None,
-    *args,
-    **kwargs,
-):
+    test: AnyCallable,
+    truesocket_recording_dir: str | None = None,
+    strict_mode: bool = False,
+    strict_mode_allowed: list[str | Address] | None = None,
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
     async with Mocketizer.factory(
         test, truesocket_recording_dir, strict_mode, strict_mode_allowed, args
     ):
