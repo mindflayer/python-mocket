cc388ee2a4e7394914ecb8035a45a2701afd2457
Giorgio Salluzzo <giorgio.salluzzo@satellogic.com>
2025-05-24 23:15:21 +0200

    Testing a UDP socket.

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -2,6 +2,7 @@ import socket
 
 import pytest
 
+from mocket import Mocket, MocketEntry, mocketize
 from mocket.socket import MocketSocket
 
 
@@ -11,3 +12,21 @@ def test_blocking_socket(blocking):
     sock.connect(("locahost", 1234))
     sock.setblocking(blocking)
     assert sock.getblocking() is blocking
+
+
+@mocketize
+def test_udp_socket():
+    host = "127.0.0.1"
+    port = 9999
+    request_data = b"ping"
+    response_data = b"pong"
+
+    Mocket.register(MocketEntry((host, port), [response_data]))
+
+    # Your UDP client code
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(request_data, (host, port))
+    data, address = sock.recvfrom(1024)
+
+    assert data == response_data
+    assert address == (host, port)
