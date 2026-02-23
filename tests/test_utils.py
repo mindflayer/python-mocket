b876453fde9bcedd11399f6ee39dfd25762b12d8
Aiudadadadf <bloxyguy6000@gmail.com>
2026-02-23 19:40:24 +0100

    tests: add tests for hexdump and hexload including ValueError on invalid hex (#320)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from unittest.mock import NonCallableMock, patch
 
 import decorator
 
-from mocket.utils import get_mocketize
+from mocket.utils import get_mocketize, hexdump, hexload
 
 
 def mock_decorator(func: Callable[[], None]) -> None:
@@ -29,3 +29,27 @@ class GetMocketizeTestCase(TestCase):
         dec.call_args_list[0].assert_compare_to((mock_decorator,), {"kwsyntax": True})
         # Second time without kwsyntax, which succeeds
         dec.call_args_list[1].assert_compare_to((mock_decorator,))
+
+
+class HexdumpTestCase(TestCase):
+    def test_hexdump_converts_bytes_to_spaced_hex(self) -> None:
+        assert hexdump(b"Hi") == "48 69"
+
+    def test_hexdump_empty_bytes(self) -> None:
+        assert hexdump(b"") == ""
+
+    def test_hexdump_roundtrip_with_hexload(self) -> None:
+        data = b"bar foobar foo"
+        assert hexload(hexdump(data)) == data
+
+
+class HexloadTestCase(TestCase):
+    def test_hexload_converts_spaced_hex_to_bytes(self) -> None:
+        assert hexload("48 69") == b"Hi"
+
+    def test_hexload_empty_string(self) -> None:
+        assert hexload("") == b""
+
+    def test_hexload_invalid_hex_raises_value_error(self) -> None:
+        with self.assertRaises(ValueError):
+            hexload("ZZ ZZ")
